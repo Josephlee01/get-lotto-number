63ea768391072bd3fa3fa515b1e9c8c78ae5d09d
josephlee01 <bluewings113@gmail.com>
2021-08-23 10:41:12 +0900

    1.0

--- getLottoNumber.js
+++ getLottoNumber.js
@@ -1,27 +1,53 @@
+const lottoForm = document.querySelector("#lotto");
+const pensionForm = document.querySelector("#pension");
+const btn = document.querySelector(".btn-lotto");
+const btnPension = document.querySelector(".btn-pension");
 
-const makeLottoNumber = () => {
-  const lotto = new Array;
-  
-  for (let i=0; i<6; i++){
-    let lottoNumber = Math.floor(Math.random()*45+1) 
-    for (let j in lotto){
-      if (lottoNumber == lotto[j]){
-        lottoNumber = Math.floor(Math.random()*45+1)
-      } 
+const result = document.querySelector(".result");
+
+const makeLottoNumber = (e) => {
+  e.preventDefault();
+  const lotto = [];
+  for (let i = 0; i < 6; i++) {
+    let lottoNumber = Math.floor(Math.random() * 45 + 1);
+    for (let j in lotto) {
+      if (lottoNumber === lotto[j]) {
+        lottoNumber = Math.floor(Math.random() * 45 + 1);
+      }
     }
     lotto.push(lottoNumber);
   }
-  lotto.sort((a,b)=> a-b);
-  document.querySelector('.number').innerHTML = lotto.join('&nbsp;&nbsp;')
+  lotto.sort((a, b) => a - b);
+  printLottoResult(lotto);
+};
 
-  document.querySelector('.form1').innerHTML = `추천 로또번호는`
-  document.querySelector('.form2').innerHTML = `입니다.`
-}
+const printLottoResult = (lotto) => {
+  const lottoPrint = lotto.join(" ");
+  result.innerText = `${lottoPrint}`;
+  pensionForm.classList.add("hidden");
+};
 
+btn.innerText = `로또 1게임 생성`;
+btn.addEventListener("click", makeLottoNumber);
 
-const print = () => {
-  const btn = document.querySelector('.btn');
-  btn.addEventListener("click", makeLottoNumber);
-}
+const makePensionNumber = (e) => {
+  e.preventDefault();
+  const pension = [];
+  for (let i = 0; i < 6; i++) {
+    let pensionNumber = Math.floor(Math.random() * 9 + 1);
+    pension.push(pensionNumber);
+  }
+  printPensionResult(pension);
+};
 
-print();
+btn.innerText = `로또 1게임 생성`;
+btn.addEventListener("click", makeLottoNumber);
+
+const printPensionResult = (pension) => {
+  const pensionPrint = pension.join(" ");
+  result.innerText = `각 조의 ${pensionPrint}`;
+  pensionForm.classList.add("hidden");
+};
+
+btnPension.innerText = `연금복권 1게임 생성`;
+btnPension.addEventListener("click", makePensionNumber);
---
+++ getPensionNumber.js
@@ -0,0 +1,6 @@
+const lottoForm = document.querySelector('#lotto')
+const pensionForm = document.querySelector('#pension')
+const btnPension = document.querySelector('.btn-pension');
+const result = document.querySelector(".result")
+
+
